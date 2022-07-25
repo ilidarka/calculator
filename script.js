8a5e0144c95249508e0e051542d0f72a69f3836e
VLadik Leshko <leshkovladik@gmail.com>
2022-07-25 16:28:09 +0300

    parser for brackets

--- script.js
+++ script.js
@@ -3,6 +3,25 @@
 const buttons = document.querySelectorAll('.buttons');
 const input = document.querySelector('.calcInput');
 
+const actions = {
+    addition : {
+        value: "+",
+        func: (a, b) => a + b
+    },
+    subtraction : {
+        value: "-",
+        func: (a, b) => a - b
+    },
+    multiplication : {
+        value: "*",
+        func: (a, b) => a * b
+    },
+    division : {
+        value: "/",
+        func: (a, b) => a / b
+    }
+}
+
 buttons.forEach((elem) => {
     elem.addEventListener('click', (event) => f(event));
 });
@@ -13,10 +32,25 @@ const f = (event) => {
 };
 
 function calculate(expression) {
-    let parsedExpression = parseExpression(expression);
-    console.log(countExpression(parsedExpression));
+    console.log(findBrackets(expression));
+}
+
+function findBrackets(expression) {
+    let parsingResult = String(expression).match(/\((\d*.\d*)\)/);
+    if (parsingResult) {
+        return parseExpression(String(expression).replace(/\((\d*.\d*)\)/, parseExpression(parsingResult[1])));
+    } else {
+        return parseExpression(expression);
+    }
 }
 
 function parseExpression(expression) {
-    return expression.split("");
+    let parsedExpression = expression.split("");
+    let answer;
+    Object.keys(actions).map((action) => {
+        if (String(parsedExpression[1]) === actions[action].value) {
+            answer = actions[action].func(Number(parsedExpression[0]), Number(parsedExpression[2]));
+        }
+    });
+    return answer;
 }
